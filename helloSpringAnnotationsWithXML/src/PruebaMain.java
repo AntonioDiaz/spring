3ad35b3c26878b2a0f0519d9ac4643779e1e3224
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-08-23 12:48:58 +0200

    Commit inicial

---
+++ helloSpringAnnotationsWithXML/src/PruebaMain.java
@@ -0,0 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ejemplos.spring.ioc.beans.ServicioRemoto;
+
+public class PruebaMain {
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ServicioRemoto servicioRemoto = applicationContext.getBean("servicioRemoto", ServicioRemoto.class);
+		System.out.println("toma----toma  : " + servicioRemoto.consultaDato());
+	}
+}
