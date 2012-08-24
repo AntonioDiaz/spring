4eb1868ca909616c25174d2292548c8c24d7e11a
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2012-08-24 13:46:50 +0200

    prueba

--- helloSpringXML/src/ejemplos/spring/ioc/beans/beanfactory/PruebaApplicationContext.java
+++ helloSpringXML/src/ejemplos/spring/ioc/beans/beanfactory/PruebaApplicationContext.java
@@ -10,6 +10,6 @@ public class PruebaApplicationContext {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 		ServicioRemoto servicio = applicationContext.getBean("servicioRemoto", ServicioRemoto.class);
-		System.out.println("El valor es " + servicio.consultaDato());
+		System.out.println("El valor es :" + servicio.consultaDato());
 	}
 }
